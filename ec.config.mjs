3e56688052f3bdaf7130d4cf1fce9907f30edb06
Satti Vamsi Krishna Reddy <vamsi3@outlook.com>
2025-05-14 22:17:57 +0530

    Add PDF guide

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,12 +1,25 @@
 // Reference: https://expressive-code.com/reference/configuration/#using-an-ecconfigmjs-
+// No typescript, see: https://github.com/expressive-code/expressive-code/issues/283
 
 /** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
-export default {
-  // themes: ['ayu-dark'],
+const config = {
+  // https://expressive-code.com/key-features/word-wrap/#configuration
+  defaultProps: {
+    wrap: true,
+    overridesByLang: {
+      'bash,ps,sh,zsh': { preserveIndent: false },
+    },
+  },
+  frames: {
+    // https://expressive-code.com/key-features/frames/#file-name-comments
+    extractFileNameFromCode: false,
+  },
   styleOverrides: {
       borderRadius: '0.5rem',
       frames: {
         shadowColor: '#124',
       },
   },
-}
+};
+
+export default config;
